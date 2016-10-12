bb833dfd0b75c457a22320d22d8ea56c3a396ca3
ferreira-alexandre <alexferreira21@gmail.com>
2016-10-12 00:21:17 -0300

    Criaçao da classe linux

--- environments/production/manifests/nodes.pp
+++ environments/production/manifests/nodes.pp
@@ -1,8 +1,29 @@
-node 'maquina1' {
+class linux {
+
+  $admintools = ['git', 'nano', 'screen']
+
+  package { $admintools:
+    ensure => 'installed'
+  }
+
+  $ntpservice = $osfamily ? {
+    'redhat' => 'ntpd',
+    'debian' => 'ntp',
+    'default' => 'ntp',
+  }
 
   file{ '/info.txt':
     ensure  => 'present',
     content => inline_template("Created by Puppey at <%= Time.now %>\n"),
+  }
+
+  package{ 'ntp' :
+    ensure => 'installed',
+  }
+
+  service{ $ntpservice:
+    ensure => 'running',
+    enable => true
   }
 
 }
