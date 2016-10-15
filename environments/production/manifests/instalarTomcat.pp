8e2b7d6f6ac5af453a5808687c6ab853f04ccba2
ferreira-alexandre <alexferreira21@gmail.com>
2016-10-15 01:03:32 -0300

    Criação da classe que instala o tomcat

---
+++ environments/production/manifests/instalarTomcat.pp
@@ -0,0 +1,12 @@
+class instalarTomcat {
+
+  tomcat::install { '/opt/tomcat8':
+    source_url => 'http://mirror.nbtelecom.com.br/apache/tomcat/tomcat-8/v8.5.6/bin/apache-tomcat-8.5.6.tar.gz'
+  }
+
+  tomcat::instance { 'tomcat8-first':
+    catalina_home => '/opt/tomcat8',
+    catalina_base => '/opt/tomcat8/first',
+  }
+
+}
