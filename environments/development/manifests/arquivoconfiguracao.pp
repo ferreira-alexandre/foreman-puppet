b3ead6c00b16837b2de92ccd7831bd888c4f987d
ferreira-alexandre <alexferreira21@gmail.com>
2016-10-26 03:13:29 -0200

    Acerto de encoding

--- environments/development/manifests/arquivoconfiguracao.pp
+++ environments/development/manifests/arquivoconfiguracao.pp
@@ -1,6 +1,6 @@
 class arquivoconfiguracao{
   file{ 'c:/configuracoes/configuracao.txt':
     ensure  => 'present',
-    content => 'Esta é uma configuração de DESENVOLVIMENTO',
+    content => 'Esta eh uma configuracao de DESENVOLVIMENTO',
   }
 }
--- environments/production/manifests/arquivoconfiguracao.pp
+++ environments/production/manifests/arquivoconfiguracao.pp
@@ -1,6 +1,6 @@
 class arquivoconfiguracao{
   file{ 'c:/configuracoes/configuracao.txt':
     ensure  => 'present',
-    content => inline_template('Esta é uma configuração de PRODUÇÂO, que é tão importante que informa as horas: <%= Time.now %>'),
+    content => inline_template('Esta eh uma configuracao de PRODUÇÂO, que é tão importante que informa as horas: <%= Time.now %>'),
   }
 }
