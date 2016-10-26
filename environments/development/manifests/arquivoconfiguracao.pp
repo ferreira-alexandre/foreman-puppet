8143b2a328bfe6b73646bfdcd605fc9dd3d3f8cd
ferreira-alexandre <alexferreira21@gmail.com>
2016-10-26 03:24:02 -0200

    Acerto na criação do arquivo

--- environments/development/manifests/arquivoconfiguracao.pp
+++ environments/development/manifests/arquivoconfiguracao.pp
@@ -1,4 +1,9 @@
 class arquivoconfiguracao{
+
+  file{ 'c:/configuracoes':
+    ensure  => 'directory',
+  }
+
   file{ 'c:/configuracoes/configuracao.txt':
     ensure  => 'file',
     content => 'Esta eh uma configuracao de DESENVOLVIMENTO',
