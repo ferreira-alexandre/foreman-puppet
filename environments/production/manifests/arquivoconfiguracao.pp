6b46a435b74de7cbacc762c67add37c32d937a32
ferreira-alexandre <alexferreira21@gmail.com>
2016-10-26 03:25:36 -0200

    Acerto na criação do arquivo

--- environments/production/manifests/arquivoconfiguracao.pp
+++ environments/production/manifests/arquivoconfiguracao.pp
@@ -1,6 +1,11 @@
 class arquivoconfiguracao{
+
+  file{ 'c:/configuracoes':
+    ensure  => 'directory',
+  }
+
   file{ 'c:/configuracoes/configuracao.txt':
-    ensure  => 'present',
+    ensure  => 'file',
     content => inline_template('Esta eh uma configuracao de PRODUCAO, que eh tao importante que informa as horas: <%= Time.now %>'),
   }
 }
